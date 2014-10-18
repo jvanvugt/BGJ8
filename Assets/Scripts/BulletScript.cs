cd6d234c10c43d678492af28c9de21269a32925a
Joris1225 <jorisvanvugt@live.nl>
2014-10-18 02:02:41 +0200

    Initial commit

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D.velocity = transform.right * 10f;
+        Destroy(gameObject, 1f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(1, 1);
+    public GameObject bullet;
+
+    private float rateOfFire = 0.25f;
+    private float lastFired = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (Time.time > lastFired + rateOfFire)
+            {
+                lastFired = Time.time;
+                GameObject b = (GameObject) Instantiate(bullet);
+                b.transform.position = transform.position;
+                b.transform.rotation = transform.rotation;
+            }
+        }
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - objectPos;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 moveDir = new Vector2(Input.GetAxisRaw("Horizontal") * speed.x, 
+                                        Input.GetAxisRaw("Vertical") * speed.y);
+        rigidbody2D.MovePosition(rigidbody2D.position + moveDir * Time.deltaTime);
+	}
+}
